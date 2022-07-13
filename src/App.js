f0eb73d667a09e2c3a9b1f94d24e241e0502b7d8
Stanislav Aleksandrov <Stanislav.Aleksandrov@a1.bg>
2022-07-13 17:35:44 +0300

    Work on layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import TaskList from './components/TasksList';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ResponsiveAppBar />
+            </header>
+
+            <main className="App-main">
+                <TaskList />
+            </main>
+
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    );
 }
 
 export default App;
